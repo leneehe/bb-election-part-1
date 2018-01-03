bfcaabdbc42807ef287d1a41391e40b54f238029
Lena <fatmeow@gmail.com>
2018-01-03 13:24:59 -0500

    Complete part 1 using AJAX request to display votes

--- election.js
+++ election.js
@@ -1,5 +1,15 @@
 document.addEventListener("DOMContentLoaded", function() {
+  var ulElection = document.querySelector('#election')
+  $.ajax({
+    url: 'https://bb-election-api.herokuapp.com/',
+    method: 'GET',
+  }).done(function(responseData){
+    responseData.candidates.forEach(function(candidate) {
+      var li = document.createElement('li')
+      li.innerHTML = '<b>' + candidate.name + ':</b> ' + candidate.votes + ' votes';
+      ulElection.append(li)
+    })
+  })
 
-  // Imagination!
 
 });
