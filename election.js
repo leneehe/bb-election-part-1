9680cec655aa6359c49763265637c2862e3c2198
Lena <fatmeow@gmail.com>
2018-01-03 13:27:04 -0500

    Specify dataType in ajax request

--- election.js
+++ election.js
@@ -3,6 +3,7 @@ document.addEventListener("DOMContentLoaded", function() {
   $.ajax({
     url: 'https://bb-election-api.herokuapp.com/',
     method: 'GET',
+    dataType: 'JSON'
   }).done(function(responseData){
     responseData.candidates.forEach(function(candidate) {
       var li = document.createElement('li')
